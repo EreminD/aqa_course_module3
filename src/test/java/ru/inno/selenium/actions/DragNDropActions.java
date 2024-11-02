a1c0553965cd9b3a1efefe2cfdf75dbcef1baacc
Dmitry Eremin <mitya1705@gmail.com>
2024-11-02 20:00:24 +0300

    selenoid

--- src/test/java/ru/inno/selenium/actions/DragNDropActions.java
+++ src/test/java/ru/inno/selenium/actions/DragNDropActions.java
@@ -6,21 +6,51 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DragNDropActions {
 
-    public static final String token = "e";
+    public static final String token = System.getProperty("trelloToken", "noToken");
     public static final String cookieName = "cloud.session.token";
 
 
     private WebDriver driver;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-        driver = new ChromeDriver(options);
+        options.setCapability("browserVersion", "128");
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            /* How to add test badge */
+            put("name", "Test badge...");
+
+            put("enableVNC", true);
+            /* How to set session timeout */
+            put("sessionTimeout", "15m");
+
+            /* How to set timezone */
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            /* How to add "trash" button */
+            put("labels", new HashMap<String, Object>() {{
+                put("manual", "true");
+            }});
+
+            /* How to enable video recording */
+            put("enableVideo", true);
+        }});
+
+
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), options);
     }
 
     @AfterEach
@@ -31,9 +61,10 @@ public class DragNDropActions {
     }
 
     @Test
-    public void sendKeysAndGetText() {
+    public void sendKeysAndGetText() throws InterruptedException {
         driver.get("https://trello.com/b/wEDviEAC/selenium");
         driver.manage().window().maximize();
+
         Cookie cookie = new Cookie(cookieName, token);
         driver.manage().addCookie(cookie);
         driver.navigate().refresh();
